b98e86e99741eec4d020eb298d7856429f683a8a
Jan Kosmala <jan.kosmala@gmail.com>
2018-07-04 22:51:20 +0200

    initial commit

---
+++ RefitDemo/IHnapi.cs
@@ -0,0 +1,14 @@
+﻿namespace RefitDemo
+{
+    using Refit;
+    using System.Collections.Generic;
+
+    public interface IHnapi
+    {
+        [Get("/item/{id}")]
+        Item GetItem(int id);
+
+        [Get("/news?page={pageId}")]
+        IEnumerable<Item> GetPage(int pageId);
+    }
+}
